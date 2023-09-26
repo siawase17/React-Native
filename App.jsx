87a1cd434dd46d54a5845bac0180e0ad1eb99136
박유진 <qkrdbw@naver.com>
2023-09-27 02:38:22 +0900

    apply styles

--- App.jsx
+++ App.jsx
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Hello, React Native!</Text>
+      <Text style={styles.text}>Hello, React Native!</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -17,4 +17,8 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  text: {
+    fontSize: 28,
+    color: 'green',
+  }
 });
