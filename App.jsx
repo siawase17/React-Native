84feaa2fb7f9c64a1271c64f443d8991fdb3a425
박유진 <qkrdbw@naver.com>
2023-09-27 16:21:19 +0900

    layout with Flexbox

--- App.jsx
+++ App.jsx
@@ -1,24 +1,12 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { View } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text style={styles.text}>Hello, React Native!</Text>
-      <StatusBar style="auto" />
+    <View style={{ flex: 1 }}>
+      <View style={{ flex: 1, backgroundColor: 'tomato'}}></View>
+      <View style={{ flex: 3, backgroundColor: 'teal'}}></View>
+      <View style={{ flex: 1, backgroundColor: 'orange'}}></View>
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  text: {
-    fontSize: 28,
-    color: 'green',
-  }
-});
